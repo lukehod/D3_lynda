d6c634d87d93013f9ca633f9bc1f6978679c8247
Luke <hodnett53@gmail.com>
2015-12-01 21:45:48 -0700

    create force example

--- script.js
+++ script.js
@@ -1,58 +1,104 @@
-var width = 400,
-    height = 400,
-    radius = 200,
-    colors = d3.scale.ordinal()
-      .range(['#595AB7','#A57706','#D11C24','#C61C6F','#BD3613','#2176C7','#259286','#738A05']);
+/* variables to hold width, height and circle width */
+var w = 400,
+    h = 400,
+    circleWidth = 5;
 
-var pieData = [
-    { label: "Barot",
-      value: 50 },
-    { label: "Gerard",
-      value: 50 },
-    { label: "Jonathan",
-      value: 50 },
-    { label: "Lorenzo",
-      value: 50 },
-    { label: "Hillary",
-      value: 50 },
-    { label: "Jennifer",
-      value: 50 }
+/* color palette to use */
+var palette = {
+      "lightgray": "#819090",
+      "gray": "#708284",
+      "mediumgray": "#536870",
+      "darkgray": "#475B62",
+      "darkblue": "#0A2933",
+      "darkerblue": "#042029",
+      "paleryellow": "#FCF4DC",
+      "paleyellow": "#EAE3CB",
+      "yellow": "#A57706",
+      "orange": "#BD3613",
+      "red": "#D11C24",
+      "pink": "#C61C6F",
+      "purple": "#595AB7",
+      "blue": "#2176C7",
+      "green": "#259286",
+      "yellowgreen": "#738A05"
+};
+
+/* array of objects for the nodes */
+var nodes = [
+    { name: "Parent" },
+    { name: "child1" },
+    { name: "child2", target: [0] },
+    { name: "child3", target: [0] },
+    { name: "child4", target: [1] },
+    { name: "child5", target: [0, 1, 2, 3] }
 ];
 
-var pie = d3.layout.pie()
-    .value(function(d) {
-        return d.value;
-    })
+/* variable to hold links */
+var links = [];
 
-var arc = d3.svg.arc()
-    .outerRadius(radius);
+/* loop through nodes and add links to array */
+for (var i = 0; i<nodes.length; i++) {
+    if (nodes[i].target !== undefined) {
+        for (var j = 0; j < nodes[i].target.length; j++) {
+            links.push({
+                source: nodes[i],
+                target: nodes[nodes[i].target[j]]
+            });
+        }
+    }
+}
 
-var myChart = d3.select('#chart').append('svg')
-    .attr('width', width)
-    .attr('height', height)
+var myChart = d3.select('#chart')
+    .append('svg')
+    .attr('width', w)
+    .attr('height', h);
+
+var force = d3.layout.force()
+    .nodes(nodes)
+    .links([])
+    .gravity(0.1)
+    .charge(-1000)
+    .size([w, h]);
+
+var link = myChart.selectAll('line')
+    .data(links)
+    .enter()
+    .append('line')
+    .attr('stroke', palette.gray);
+
+var node = myChart.selectAll('circle')
+    .data(nodes)
+    .enter()
     .append('g')
-    .attr('transform', 'translate(' +(width - radius)+ ',' +(height - radius)+ ')')
-    .selectAll('path')
-    .data(pie(pieData))
-    .enter().append('g')
-        .attr('class', 'slice');
+    .call(force.drag);
 
-var slices = d3.selectAll('g.slice')
-    .append('path')
-    .attr('fill', function (d, i) {
-        return colors(i);
+node.append('circle')
+    .attr('cx', function(d) {
+        return d.x;
     })
-    .attr('d', arc);
+    .attr('cy', function(d) {
+        return d.y;
+    })
+    .attr('r', circleWidth)
+    .attr('fill', palette.pink);
 
-var text = d3.selectAll('g.slice')
-    .append('text')
-    .text(function(d, i) {
-        return d.data.label;
-    })
-    .attr('text-anchor', 'middle')
-    .attr('fill', 'white')
-    .attr('transform', function(d) {
-        d.innerRadius = 0;
-        d.outerRadius = radius;
-        return 'translate(' +arc.centroid(d)+ ')';
+force.on('tick', function(e) {
+    node.attr('transform', function(d, i) {
+        return 'translate(' +d.x+ ', ' +d.y+ ')';
     });
+
+    link.attr('x1', function(d) {
+        return d.source.x;
+    })
+    .attr('y1', function(d) {
+        return d.source.y;
+    })
+    .attr('x2', function(d) {
+        return d.target.x;
+    })
+    .attr('y2', function(d) {
+        return d.target.y;
+    });
+});
+
+force.start();
